58d474eceedc1faac47b3271e0a151b639cbc41b
vrushank <ramanivrushank264@gmail.com>
2025-11-23 19:11:35 +0530

    Add files via upload

--- Post Lab 20/Example_code.py
+++ Post Lab 20/Example_code.py
@@ -1,8 +1,6 @@
 import soundfile as sf
-
-
 # Load audio file
-audio, sample_rate = sf.read(r'C:\Users\Slock\OneDrive\Desktop\song.wav') # Write audio file
+audio, sample_rate = sf.read(r'C:\Users\Vrushank\OneDrive\Desktop\song.wav') # Write audio file
 sf.write('new_audio_file.wav', audio, sample_rate) 
 import matplotlib.pyplot as plt
 import numpy as np  
